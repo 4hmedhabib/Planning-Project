9b146cbb752a8ca43d06f6d2cd987d70d0723ed1
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-10-05 20:28:32 +0300

    initialized using redux

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,22 +1,5 @@
 const ProjectList = () => {
-	return (
-		<div className="project-list section container ">
-			<div className="card z-depth-0 project-summary">
-				<div className=" card-content grey-text text-darken-3">
-					<span className="card-title">Project Title</span>
-					<p>Posted by the Ahmed Habib</p>
-					<p className="grey-text">3rd September</p>
-				</div>
-			</div>
-			<div className="card z-depth-0 project-summary">
-				<div className=" card-content grey-text text-darken-3">
-					<span className="card-title">Project Title</span>
-					<p>Posted by the Ahmed Habib</p>
-					<p className="grey-text">3rd September</p>
-				</div>
-			</div>
-		</div>
-	);
+	return <div className="project-list section  " />;
 };
 
 export default ProjectList;
