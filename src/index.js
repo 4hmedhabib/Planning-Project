a071162dcd2094dd6159db755628162912d5d4a7
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-10-05 20:12:20 +0300

    initialized redux and react-redux file

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
