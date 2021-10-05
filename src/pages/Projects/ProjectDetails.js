e48b209288e06b15b99ba63f5f7e57483bc2b3c9
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-10-05 17:41:43 +0300

    added project details

---
+++ src/pages/Projects/ProjectDetails.js
@@ -0,0 +1,22 @@
+const ProjectDetails = () => {
+	return (
+		<div className="container section project-details">
+			<div className="card z-depth-0">
+				<div className="card-content">
+					<span className="card-title">Prject title</span>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia odit mollitia incidunt nulla
+						excepturi ut repellat earum dicta nisi. Perspiciatis, obcaecati! Neque eaque quaerat quisquam
+						veniam maxime soluta natus? Cum.
+					</p>
+				</div>
+				<div className="card-action grey lighten-4 grey-text">
+					<div>Posted by The Ahmed Habib</div>
+					<div>2nd September 2021</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectDetails;
